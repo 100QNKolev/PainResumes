497791100399b3de9f1f2d01ad01d4c594bed184
100QN <83006473+100QNKolev@users.noreply.github.com>
2023-07-04 17:10:46 +0300

    Added Header

---
+++ pain-resumes/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+//TODO: Implement AuthContext
+import { Link } from 'react-router-dom';
+
+import styles from './Header.module.css';
+
+export const Header = () => {
+    const isAuthenticated = false;
+    const userId = 1;
+
+    return (
+        <nav>
+            <ul className={styles['list-item']}>
+                <li> <Link to="/templates">Resume Templates</Link> </li>
+                {!isAuthenticated && (
+                    <>
+                        <li id="guest"> <Link to="/login">Login</Link> </li>
+                        <li id="guest"> <Link to="/register">Register</Link> </li>
+                    </>
+                )}
+                {isAuthenticated && (
+                    <>
+                        <li id="user"> <Link to={`/account/${userId}`}>My account</Link> </li>
+                        <li id="user"> <Link to="/logout">Logout </Link> </li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    )
+}
