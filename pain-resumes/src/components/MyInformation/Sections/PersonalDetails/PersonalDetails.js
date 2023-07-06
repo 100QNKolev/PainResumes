578f5c91a29fab7dfd7c1d262ab78b6c1c8ab74f
100QN <83006473+100QNKolev@users.noreply.github.com>
2023-07-06 13:04:31 +0300

    Added Personal Details section
    The section has no functionality!

---
+++ pain-resumes/src/components/MyInformation/Sections/PersonalDetails/PersonalDetails.js
@@ -0,0 +1,52 @@
+import { useForm } from '../../../../hooks/useForm';
+
+import styles from './PersonalDetails.module.css';
+
+export const PersonalDetails = ({ onSubmitHandler }) => {
+    const { values, changeHandler, onSubmit } = useForm({
+        firstName: ''
+        , lastName: ''
+        , age: ''
+        , phone: ''
+        , adress: ''
+        , email: ''
+        , profile: ''
+    }, onSubmitHandler);
+
+    return (
+        <form onSubmit={onSubmit} method='POST'>
+            <h1>Personal Details</h1>
+            <div className={styles['nestedContainer']}>
+                <div className={styles['inputDiv']}>
+                    <label>First Name:</label>
+                    <input type="text" placeholder="First Name" value={values.firstName} onChange={changeHandler} name="firstName" id="firstName" className={styles['currentInput']} />
+                </div>
+                <div className={styles['inputDiv']}>
+                    <label>Last Name:</label>
+                    <input type="text" placeholder="Last Name" value={values.lastName} onChange={changeHandler} name="lastName" id="lastName" className={styles['currentInput']} />
+                </div>
+                <div className={styles['inputDiv']}>
+                    <label>Age:</label>
+                    <input type="text" placeholder="Age" value={values.age} onChange={changeHandler} name="age" id="age" className={styles['currentInput']} />
+                </div>
+                <div className={styles['inputDiv']}>
+                    <label>Phone:</label>
+                    <input type="text" placeholder="Phone" value={values.phone} onChange={changeHandler} name="phone" id="phone" className={styles['currentInput']} />
+                </div>
+                <div className={styles['inputDiv']}>
+                    <label>Adress:</label>
+                    <input type="text" placeholder="Adress" value={values.adress} onChange={changeHandler} name="adress" id="adress" className={styles['currentInput']} />
+                </div>
+                <div className={styles['inputDiv']}>
+                    <label>Email:</label>
+                    <input type="text" placeholder="Email" value={values.email} onChange={changeHandler} name="email" id="email" className={styles['currentInput']} />
+                </div>
+                <div className={styles['inputDiv']}>
+                    <h2>Profile:</h2>
+                    <textarea placeholder="e.g. Dedicated football coach with 10 years of experience" value={values.profile} onChange={changeHandler} name="profile" id="profile" />
+                </div>
+                <button className={styles['saveBtn']}>Save</button>
+            </div>
+        </form>
+    );
+};
