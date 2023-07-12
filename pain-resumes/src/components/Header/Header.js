231d43993a433430b44ca4412f2eb2f242379cac
Ivan Zlatinov <ivanzlatinov006@gmail.com>
2023-07-12 14:13:23 +0300

    Revert "login/register/header fix"
    This reverts commit 2b3f130f55a9fe76429f6004e57b19eb5f0510f1.

--- pain-resumes/src/components/Header/Header.js
+++ pain-resumes/src/components/Header/Header.js
@@ -8,28 +8,25 @@ export const Header = () => {
     const { isAuthenticated, userId } = useAuthContext();
 
     return (
-       <nav>
-           
+        <nav>
+            <ul className={styles['list-item']}>
+                <li> <Link to='/templates'>Resume Templates</Link> </li>
+
                 {!isAuthenticated && (
                     <>
-                        <header>
-                            <h2 className = {styles['logo']}></h2>
-                            <nav className = {styles['navigation']}>
-                                <a href = "/templates">Resume Templates</a>
-                                <a href = "Login">Login</a>
-                                <a href = "Register">Register</a>
-                            </nav>
-                        </header>
+                        <li id='guest'> <Link to='/login'>Login</Link> </li>
+                        <li id='guest'> <Link to='/register'>Register</Link> </li>
                     </>
                 )}
+
                 {isAuthenticated && (
                     <>
                         <li id='user'> <Link to={`/account/${userId}`}>My Information</Link> </li>
-                        <li id='user'> <Link to='/templates'>Resume Templates</Link> </li>
-                        <li id='user'> <Link to={`/account/${userId}`}>My account</Link> </li>
                         <li id='user'> <Link to='/logout'>Logout </Link> </li>
                     </>
                 )}
+
+            </ul>
         </nav>
     );
 };
